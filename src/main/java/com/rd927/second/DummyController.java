1def3312b264108bb55c6a79b58e0b4ad8d8325e
rdarshan927 <rdarshan927@gmail.com>
2024-11-12 09:25:44 +0530

    first commit

---
+++ src/main/java/com/rd927/second/DummyController.java
@@ -0,0 +1,11 @@
+package com.rd927.second;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class DummyController {
+    @GetMapping("/home")
+    public String home() {
+        return "Welcome to the Home Page!";
+    }
+}
---
+++ src/test/java/com/rd927/second/SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rd927.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
